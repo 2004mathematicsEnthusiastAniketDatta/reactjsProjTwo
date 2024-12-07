909aa22fc6d93719067cbd2498c1c8ebd2b4deb9
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-12-07 07:09:03 +0000

    sidebars

--- recipie/src/Components/Sidebar.jsx
+++ recipie/src/Components/Sidebar.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='p-5 shadow-lg w-48 '>
+     <h1 className="font-bold text-base">
+      <ul>
+        <li>Spicy Foods</li>
+        <li>Sweet Dishes</li>
+        <li>Sour Dishes</li>
+        <li>Salty Dishes</li>
+      </ul>
+     </h1>
+     <h1 className='font-bold pt-5 text-lg'>Subscriptions</h1>
+     <h1 className="font-bold text-base">
+      <ul>
+        <li>Trending</li>
+        <li>Favourites</li>
+        <li>New dishes</li>
+        <li>Healthy</li>
+      </ul>
+     </h1>
+    </div>
   )
 }
 
