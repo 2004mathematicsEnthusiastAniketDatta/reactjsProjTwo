44a8276332fc0b7eabdeaa8e9e8e570ad685b3ee
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-12-11 15:28:07 +0000

    color

--- recipie/src/App.jsx
+++ recipie/src/App.jsx
@@ -6,14 +6,21 @@ import Sidebar from './Components/Sidebar'
 import Body from './Components/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colorState, setColorState] = useState("white");
 
   return (
     <>
-      <div className='text-5xl'>
+      <div className='text-5xl  w-full h-screen duration-200' style={{backgroundColor: colorState}}>
         <Head />
         <Body />
-        
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <button 
+            className={`px-6 py-2 rounded-full text-white ${colorState === "white" ? "bg-blue-500" : "bg-gray-900"} hover:bg-${colorState === "white" ? "blue-600" : "gray-800"} transition duration-200`}
+            onClick={() => setColorState(colorState === "white" ? "orange" : "white")}
+          >
+            Switch Theme
+          </button>  {/* Switch Theme Button */}
+        </div>
         {/* 
           * Head
           * Body
